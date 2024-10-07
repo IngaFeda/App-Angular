ca234ea7e1bc87e25cf32d8a61ce025fdb44e7e8
Inga <122355289+IngaFeda@users.noreply.github.com>
2024-10-07 08:46:14 +0000

    changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, input, Input, output, Output, } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { type User } from './user.model';
+// import { DUMMY_USERS } from '../dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 // type User =  {
@@ -7,12 +8,6 @@ import { DUMMY_USERS } from '../dummy-users';
 //     avatar: string;
 //     name: string
 //   }
-
-  interface User  {
-      id: string;
-      avatar: string;
-      name: string
-    }
 
 @Component({
   selector: 'app-user',
@@ -29,6 +24,7 @@ export class UserComponent {
   // @Input({required: true}) name!:string;
 
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   // {
   //   id: string;
   //   avatar: string;
